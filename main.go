13ce1844822761930837a82bdc7f953256550e56
antgrutta <antgrutta@github.com>
2025-07-31 11:31:08 -0400

    Optimizing workflows further

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "github.com/mona-actions/gh-migration-monitor/cmd"
 
+// Version info set by ldflags during build
+var (
+	version   = "dev"
+	buildDate = "unknown"
+)
+
 func main() {
+	cmd.SetVersionInfo(version, buildDate)
 	cmd.Execute()
 }
